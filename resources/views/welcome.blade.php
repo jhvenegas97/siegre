a1a0f8a82df5b9d0c0dc99ad889118f9af2add99
Daniela270795 <91349543+Daniela270795@users.noreply.github.com>
2022-04-13 09:13:40 -0500

    Cambio menu inicio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,9 +8,9 @@
             <div class="d-flex carousel-item active align-items-center">
                 <img src="{{asset('images/Egresados.png')}}" class="d-block w-100" alt="egresados">
                 <div class="carousel-caption d-none d-md-block text-start">
-                    <h1>Bienvenidos a SIEGRE</h1>
+                    <h1>Bienvenidos a SIEGRES</h1>
                     <h3>Sistema de Información Web para Egresados</h3>
-                    <h4>Para conocer más información registrese <a href="{{route('login')}}">aquí</a></h4>
+                    <h4>Para conocer más información regístrese <a href="{{route('login')}}">aquí</a></h4>
                 </div>
             </div>
         </div>
@@ -34,7 +34,7 @@
             <div class="accordion-item">
                 <h2 class="accordion-header" id="panelsStayOpen-headingOne">
                     <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                        <h3 class="mb-0"><strong><span class="badge bg-secondary btn-new">¿Qué es SIEGRE?</span></strong></h3>
+                        <h3 class="mb-0"><strong><span class="badge bg-secondary btn-new">¿Qué es SIEGRES?</span></strong></h3>
                     </button>
                 </h2>
                 <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
@@ -46,26 +46,27 @@
             <div class="accordion-item">
                 <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
                     <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-                        <h3 class="mb-0"><strong><span class="badge bg-secondary btn-new">¿Qué encontraras en SIEGRE?</span></strong></h3>
+                        <h3 class="mb-0"><strong><span class="badge bg-secondary btn-new">¿Qué encontrarás en SIEGRES?</span></strong></h3>
                     </button>
                 </h2>
                 <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingTwo">
                     <div class="accordion-body">
                         <h5>Capacitaciones</h5>
-                        <p>Encontraras notificacione de las capacitaciones que se pueden presentar momentaniamente y que pueden ser de gran interes e innovacion para los egresados del programa Licenciatura en Informatica.
+                        <p>Encontrarás notificaciones de las capacitaciones que se pueden presentar momentaniamente y que pueden ser de gran interés e innovación para los egresados del programa Licenciatura en Informática.
                         </p>
 
                         <h5>Ofertas laborales</h5>
-                        <p>Se subiran al sistema de informacion web todas las vacantes que requieran las instituciones, emprasas u organizaciones relacionadas con el perfil ocupacional de un licenciado en informatica. Adicionalmente si eres egresado particual
-                            y estas registrado en el sistema puedes solicitar una vacante para tu institucion, empresa u organizacion.
+                        <p>Los Egresados tendrán la posibilidad de subir su hoja de vida al sistema de información para ser considerados como posibles candidatos ante las demandas
+                            laborales externas. Además, podrán publicar ofertas laborales relacionadas con el perfil laboral del Licenciado en informática.
+                           
                         </p>
 
                         <h5>Entretenimiento</h5>
-                        <p>Se puede visualisar todos los articulos de la revista runin del programa, eventos academicos entre otros aspectos de interes.
+                        <p>Se puede visualizar todos los artículos de la revista runin del programa, eventos académicos entre otros aspectos de interés.
                         </p>
 
                         <h5>Reconocimeinto al mérito</h5>
-                        <p>periodicamente se destacara a los egresados que tengan una experiencia academica y de formacion continua, con el animo de motivar a los demas a que sigan formandose.</p>
+                        <p>Periódicamente se destacará a los egresados que tengan una experiencia académica y de formación continúa, con el ánimo de motivar a los demás a que sigan formandose.</p>
                     </div>
                 </div>
             </div>
