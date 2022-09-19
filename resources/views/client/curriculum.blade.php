886a93536b5a1227577df941eccde198e8f6cbe8
Jonathan Venegas <jonathan.9129@gmail.com>
2022-09-18 22:32:11 -0500

    Add profilephotos to Curriculum View

--- resources/views/client/curriculum.blade.php
+++ resources/views/client/curriculum.blade.php
@@ -26,7 +26,7 @@
                                         class="img-responsive img-circle" alt="">
                                 @else
                                     @if ($userCurriculum[0]->fileName != null)
-                                        <img src="{{ asset('uploads/' . $userCurriculum[0]->fileName) }}" width="100"
+                                        <img src="{{ asset('uploads/profilephotos/' . $userCurriculum[0]->fileName) }}" width="100"
                                             height="100" class="img-responsive img-circle" alt="">
                                     @else
                                         <img src="{{ asset('images/admin.svg') }}" width="100" height="100"
